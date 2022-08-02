25111bd8e41331226008210e951ab1ebd78aef57
Thanos Sam <a.samaras@pmmretail.com>
2022-08-02 12:43:20 +0300

    Add repository / service / domain packages , main class and BaseModel class

---
+++ src/main/java/com/acmeflix/AcmeflixApplication.java
@@ -0,0 +1,12 @@
+package com.acmeflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcmeflixApplication.class, args);
+    }
+}
