48aa2d3bc894625ae4ca61777e9d43d286c4d258
a.kapsouros <angeloskaps@gmail.com>
2022-08-02 16:24:03 +0300

    add account class

--- src/main/java/com/acmeflix/domain/Account.java
+++ src/main/java/com/acmeflix/domain/Account.java
@@ -1,4 +1,15 @@
 package com.acmeflix.domain;
-
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import java.util.List;
+@Data
+@SuperBuilder
 public class Account {
+    private String username;
+    private String password;
+    private String email;
+    private String subscriptionPlan;
+    private String fullName;
+    private List<Profile> profiles;
+    private String phone;
 }
